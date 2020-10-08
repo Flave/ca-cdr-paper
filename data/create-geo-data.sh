5e6b28667296698a0c5f076d55ae9d3cb74bdaf7
flaviogortana <flaviogortana@gmail.com>
2020-10-08 17:27:45 +0200

    added world data

--- data/create-geo-data.sh
+++ data/create-geo-data.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 shp2json world-shp/ne_10m_admin_0_countries.shp > ./world.geojson
-node merge-countries.js
-toposimplify -p 0.3 -f < regions.topojson > ../regions.topojson
+geo2topo countries=world.geojson > world.topojson
+toposimplify -p 0.1 -f < world.topojson | topoquantize 1e4 > ./world-simplified.topojson
+
+#node merge-countries.js
+#toposimplify -p 0.3 -f < regions.topojson > ../regions.topojson
